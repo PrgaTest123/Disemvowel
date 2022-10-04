5beae338b8ae63664b5db5fe3cf06642234550a5
PrgaTest123 <marintest0@gmail.com>
2022-10-04 22:15:38 +0200

    Add project files.

---
+++ Disemvowel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Disemvowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a sentence: ") ;
+            string str = Console.ReadLine();
+            Console.WriteLine(Disemvowel(str));
+        }
+
+        public static string Disemvowel(string str)
+        {
+            //Best practice
+            //return string.Concat(str.Where(ch=> !"aeiouAEIOU".Contains(ch)));
+            
+            List<char> charsToRemove = new List<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            foreach(char c in charsToRemove)
+            {
+                str = str.Replace(c.ToString(), String.Empty);
+            }
+            return str;
+        }
+    }
+}
